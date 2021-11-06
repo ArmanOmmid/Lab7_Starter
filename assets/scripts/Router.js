b4bc0298f66d819dc40842c734f85856c8cdc9cd
Arman Ommid <armanommid@gmail.com>
2021-11-05 23:02:59 -0700

    updates

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,8 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +67,10 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+    if(!this.navigate){
+      console.log("Error"); 
+      return; 
+    }
+    
   }
 }
